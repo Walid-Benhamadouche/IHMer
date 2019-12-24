795e57aa07360d27cfec2659100e836d016e36e0
Valar <drakkendusk@gmail.com>
2019-12-24 17:07:12 +0100

    khaluta w sayi

--- src/IHM/dbConnection.java
+++ src/IHM/dbConnection.java
@@ -10,7 +10,7 @@ public class dbConnection {
         try
         {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ihmer","root","");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ihmer?autoReconnect=true&useSSL=false","root","");
             return con;
         }catch(Exception e){ System.out.println(e);}
         return null;
